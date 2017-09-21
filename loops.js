5a393d528cb6aaf29d43ba72ec3020de408038ae
maude sos <mualth@gmail.com>
2017-09-21 01:22:18 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,7 +3,12 @@ var forLoop = () => {
   const array=[]
   const t = Math.floor(Math.random() * 10)
   for (let i=0; i<t; i++) {
-    array.push("I am ${i} strange loop${i=== 0 ? '' : 's'} .");
+    if (i === 1) {
+      array.push("I am 1 strange loop.");
+    } else {
+      array.push("I am ${i} strange loops.")
+    }
   }
   return [array, t]
 }
+
