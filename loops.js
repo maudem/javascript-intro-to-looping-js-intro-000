00eab74824b53e8c3761e08b9b8569cb9f2223e7
maude sos <mualth@gmail.com>
2017-09-21 00:56:48 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,7 +2,10 @@
 const makeArray = (array) => {
   const array=[]
   const t = Math.floor(Math.random() * 10)
-  for (let i=0; i<25; i++) {
+  for (let i=0; i<t; i++) {
     array.push("I am ${i} strange loop${i=== 0 ? '' : 's'} .");
   }
+  return (array, t)
 }
+
+makeArray();
