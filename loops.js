3b0660d4f7cd784f3eea9ecdcb2ac6d82a493181
maude sos <mualth@gmail.com>
2017-09-21 00:51:27 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,5 +1,5 @@
 var makeArray = (array) => {
   for (let i=0; i<t; i++) {
-    array.push("I am ${i} strange loop${i=== 0 ? '' : 's'} .");
+    array.push('I am ${i} strange loop${i=== 0 ? '' : 's'} .');
   }
 }
