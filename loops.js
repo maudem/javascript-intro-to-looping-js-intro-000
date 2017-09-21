9c934c1f99831975c7d2b668cba699c5364070f9
maude sos <mualth@gmail.com>
2017-09-21 01:22:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -6,8 +6,7 @@ var forLoop = () => {
     if (i === 1) {
       array.push("I am 1 strange loop.");
     } else {
-      array.push("I am ${i} strange loops.")
+      array.push("I am ${i} strange loops.");
     }
   }
-  return [array, t]
 }
