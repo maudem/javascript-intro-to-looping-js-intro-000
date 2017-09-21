3abf36cf0f345032879dc37deaaabd2db7c99b9f
maude sos <mualth@gmail.com>
2017-09-21 01:10:57 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -4,6 +4,6 @@ var forLoop = () => {
   const t = Math.floor(Math.random() * 10)
   for (let i=0; i<t; i++) {
     array.push("I am ${i} strange loop${i=== 0 ? '' : 's'} .");
-    return array
   }
+  return [array, t]
 }
